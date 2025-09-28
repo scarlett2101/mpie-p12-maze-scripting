f2bdf16a5f966ea593be37322f78ba3197561313
scarlett2101 <xtn516@york.ac.uk>
2025-09-28 15:00:42 +0100

    tilts

--- Assets/tiltingScript.cs
+++ Assets/tiltingScript.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class tiltingScript : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,13 @@ public class tiltingScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float rotationVertical = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * speed;
+        rotationVertical *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Rotate(rotation, 0, 0);
+        transform.Rotate(0, 0, rotationVertical);
+
         
     }
 }
